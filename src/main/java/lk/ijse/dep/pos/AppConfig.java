d7b5722b05c683f72a070d71f8753727a7948a89
LawrenceAsanka <asankalorance@gmail.com>
2020-09-06 17:10:18 +0530

    Import HibernateConfig class to AppConfig class

--- src/main/java/lk/ijse/dep/pos/AppConfig.java
+++ src/main/java/lk/ijse/dep/pos/AppConfig.java
@@ -2,8 +2,11 @@ package lk.ijse.dep.pos;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
-@ComponentScan
+@ComponentScan("lk.ijse.dep.pos")
 @Configuration
+@Import(HibernateConfig.class)
 public class AppConfig {
+
 }
