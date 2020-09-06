7866db138cbce272b851ca8badb4c0d3471106e3
LawrenceAsanka <asankalorance@gmail.com>
2020-09-06 17:36:23 +0530

    Create sessionFactory

--- src/main/java/lk/ijse/dep/pos/HibernateConfig.java
+++ src/main/java/lk/ijse/dep/pos/HibernateConfig.java
@@ -1,7 +1,23 @@
 package lk.ijse.dep.pos;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class HibernateConfig {
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory(DataSource ds){
+        LocalSessionFactoryBean lsfb = new LocalSessionFactoryBean();
+        //set the data source
+        lsfb.setDataSource(ds);
+        // set entity package to identify the entities
+        lsfb.setPackagesToScan("lk.ijse.dep.pos.entity");
+        lsfb.setHibernateProperties();
+        return lsfb;
+    }
 }
+
