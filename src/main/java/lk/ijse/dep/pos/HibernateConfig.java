bab47947bc8a90af83f8109501d26c6bc1608caf
LawrenceAsanka <asankalorance@gmail.com>
2020-09-06 22:54:02 +0530

    Add Transaction Manager

--- src/main/java/lk/ijse/dep/pos/HibernateConfig.java
+++ src/main/java/lk/ijse/dep/pos/HibernateConfig.java
@@ -1,16 +1,21 @@
 package lk.ijse.dep.pos;
 
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
+@EnableTransactionManagement
 public class HibernateConfig {
 
     @Autowired
@@ -45,6 +50,11 @@ public class HibernateConfig {
         properties.put("hibernate.hbm2ddl.auto","update");
         properties.put("hibernate.allow_refresh_detached_entity",false);
         return properties;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(SessionFactory sf){
+        return new HibernateTransactionManager(sf);
     }
 }
 
