f1b243b3b95fc23a88063780f33a4990b40ad781
LawrenceAsanka <asankalorance@gmail.com>
2020-09-06 18:51:22 +0530

    Add hibernateProperties()

--- src/main/java/lk/ijse/dep/pos/HibernateConfig.java
+++ src/main/java/lk/ijse/dep/pos/HibernateConfig.java
@@ -8,6 +8,7 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 public class HibernateConfig {
@@ -22,7 +23,8 @@ public class HibernateConfig {
         lsfb.setDataSource(ds);
         // set entity package to identify the entities
         lsfb.setPackagesToScan("lk.ijse.dep.pos.entity");
-        lsfb.setHibernateProperties();
+        // set properties
+        lsfb.setHibernateProperties(hibernateProperties());
         return lsfb;
     }
 
@@ -32,8 +34,17 @@ public class HibernateConfig {
         ds.setDriverClassName(env.getRequiredProperty("hibernate.connection.driver_class"));
         ds.setUrl(env.getRequiredProperty("hibernate.connection.url"));
         ds.setUsername(env.getRequiredProperty("hibernate.connection.username"));
-        ds.setPassword(env.getRequiredProperty("hibernate.connection.password"));
+        ds.setPassword(env.getRequiredProperty("hibernate.connection.password "));
         return ds;
+    }
+
+    private Properties hibernateProperties(){
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect","org.hibernate.dialect.MySQL8Dialect");
+        properties.put("hibernate.show_sql",true);
+        properties.put("hibernate.hbm2ddl.auto","update");
+        properties.put("hibernate.allow_refresh_detached_entity",false);
+        return properties;
     }
 }
 
