4e600e55b8adebc3baf2156f29ac2eb9c59f293b
LawrenceAsanka <asankalorance@gmail.com>
2020-09-06 22:31:17 +0530

    Rewamp QueryDAOImpl

--- src/main/java/lk/ijse/dep/pos/dao/custom/impl/QueryDAOImpl.java
+++ src/main/java/lk/ijse/dep/pos/dao/custom/impl/QueryDAOImpl.java
@@ -4,7 +4,9 @@ import lk.ijse.dep.pos.dao.custom.QueryDAO;
 import lk.ijse.dep.pos.entity.CustomEntity;
 
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.transform.Transformers;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -12,12 +14,13 @@ import java.util.List;
 @Component
 public class QueryDAOImpl implements QueryDAO {
 
-    private  Session session;
+    @Autowired
+    private SessionFactory sessionFactory;
 
     @Override
     public List<CustomEntity> getOrderDetail() throws Exception {
 
-       return session.createNativeQuery("SELECT o.id AS orderId,o.date AS orderDate,c.id AS customerId,c.name AS customerName,(SUM(od.qty*od.unitPrice)) AS total FROM `Order` o\n" +
+       return getSession().createNativeQuery("SELECT o.id AS orderId,o.date AS orderDate,c.id AS customerId,c.name AS customerName,(SUM(od.qty*od.unitPrice)) AS total FROM `Order` o\n" +
                 "INNER JOIN OrderDetail od on o.id = od.orderId\n" +
                 "INNER JOIN Customer c on o.customerId = c.id\n" +
                 "GROUP BY o.id").setResultTransformer(Transformers.aliasToBean(CustomEntity.class)).list();
@@ -26,6 +29,6 @@ public class QueryDAOImpl implements QueryDAO {
 
     @Override
     public Session getSession() {
-        return null;
+        return sessionFactory.getCurrentSession();
     }
 }
